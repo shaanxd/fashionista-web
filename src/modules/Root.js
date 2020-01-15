857b91830e30ceb81cfc28c94b41759e5054796f
Shahid <shaahid.xd@gmail.com>
2020-01-15 20:30:43 +0530

    Initial commit

---
+++ src/modules/Root.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import styles from './Root.module.css';
+
+const Root = props => {
+  return <div className={styles.root}>This is Fashionista</div>;
+};
+
+export default Root;
