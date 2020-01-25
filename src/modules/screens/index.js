49c57176ccc874b286279ddaa543d722ca0164be
Shahid <shaahid.xd@gmail.com>
2020-01-25 17:24:55 +0530

    Move AuthHome to Home

--- src/modules/screens/index.js
+++ src/modules/screens/index.js
@@ -2,6 +2,6 @@ export { default as Signin } from './signin/Signin';
 export { default as Signup } from './signup/Signup';
 export { default as Signout } from './signout/Signout';
 export { default as Product } from './product/Product';
-export { default as Home } from './authHome/AuthHome';
+export { default as Home } from './home/Home';
 export { default as AddProduct } from './addProduct/AddProduct';
 export { default as AddTag } from './addTag/AddTag';
