ae9f9c60b2fa12440da49b7558e256f8af494884
Shahid <shaahid.xd@gmail.com>
2020-01-28 22:38:44 +0530

    Implement collapsible component

--- src/modules/screens/checkout/Checkout.js
+++ src/modules/screens/checkout/Checkout.js
@@ -1,8 +1,22 @@
 import React from 'react';
+import { Collapse } from 'react-collapse';
+import { useMergedState } from '../../utils/useMergedState';
 
+import './Checkout.css';
 import styles from './Checkout.module.css';
 
 const Checkout = props => {
+  const [state, setState] = useMergedState({
+    step: 0
+  });
+
+  const forward = () => {
+    setState(prevState => ({ ...prevState, step: prevState.step + 1 }));
+  };
+  const backward = () => {
+    setState(prevState => ({ ...prevState, step: prevState.step - 1 }));
+  };
+
   return (
     <div className={styles.main__div}>
       <div className={styles.parent__div}>
@@ -10,12 +24,30 @@ const Checkout = props => {
           <div className={styles.header__container}>
             <span className={styles.header__text}>SHIPPING INFORMATION</span>
           </div>
-          <div className={styles.details__container}></div>
+          <Collapse isOpened={state.step === 0}>
+            <div className={styles.details__container}>
+              <button onClick={forward}>Go to payment!</button>
+            </div>
+          </Collapse>
           <div className={styles.header__container}>
             <span className={styles.header__text}>PAYMENT INFORMATION</span>
           </div>
-          <div className={styles.payment__container}></div>
+          <Collapse isOpened={state.step === 1}>
+            <div className={styles.payment__container}>
+              <button onClick={backward}>Go to shipping!</button>
+              <button onClick={forward}>Go to Confirm!</button>
+            </div>
+          </Collapse>
+          <div className={styles.header__container}>
+            <span className={styles.header__text}>CONFIRM ORDER</span>
+          </div>
+          <Collapse isOpened={state.step === 2}>
+            <div className={styles.payment__container}>
+              <button onClick={backward}>Go to shipping!</button>
+            </div>
+          </Collapse>
         </div>
+        <div className={styles.seperator__div} />
         <div className={styles.cart__div}>
           <div className={styles.header__container}>
             <span className={styles.header__text}>YOUR ORDER</span>
